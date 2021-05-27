354e576d6c6f084d647a15f67eb468afa90c0ff0
Qingmin Liu <calmarrow@gmail.com>
2021-05-27 19:45:27 +0800

    toolbox: bin: add checkpath depth parameter
    Usage: checkpatch.sh <1-n>
    It stands for the commit number to be checked.
    
    Signed-off-by: Qingmin Liu <calmarrow@gmail.com>

--- bin/checkpatch.sh
+++ bin/checkpatch.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+depth=$1
+if [ -z $depth ]; then
+  depth=1
+fi
+
 filepath=$(cd "$(dirname $0)"; pwd)
 echo $filepath
 ROOT_DIR=$filepath/checkpatch
 DPDK_CHECKPATCH_PATH=$ROOT_DIR/checkpatch.pl;
 export DPDK_CHECKPATCH_PATH
 
-$ROOT_DIR/checkpatches.sh -v -n 1
+$ROOT_DIR/checkpatches.sh -v -n $depth
